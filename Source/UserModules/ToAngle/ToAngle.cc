ab27f2cabf9037313e87544985f7d67a7875cfb1
Shan Langlais <shan.langlais@gmail.com>
2016-09-29 14:28:05 +0200

    nutting

--- Source/UserModules/ToAngle/ToAngle.cc
+++ Source/UserModules/ToAngle/ToAngle.cc
@@ -8,7 +8,7 @@ void ToAngle::Init()
 }
 
 void ToAngle::Tick() {
-  output[0][0] = atan2(input_array[0][1] - input_array[0][3], input_array[0][0] - input_array[0][2]);
+  output[0][0] = atan2(input[0][1] - input[0][3], input[0][0] - input[0][2]);
   output[0][0] = output[0][0] * (float)180.0/(float)3.1415;
 }
 
