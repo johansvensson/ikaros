524c9247de9e4b2e6c417a9db60dcc00ad48f674
Shan Langlais <shan.langlais@gmail.com>
2016-09-29 16:29:16 +0200

    fixed stuff

--- Source/UserModules/ToAngle/ToAngle.cc
+++ Source/UserModules/ToAngle/ToAngle.cc
@@ -10,6 +10,7 @@ void ToAngle::Init()
 void ToAngle::Tick() {
   output[0][0] = atan2(input[0][1] - input[0][3], input[0][0] - input[0][2]);
   output[0][0] = output[0][0] * (float)180.0/(float)3.1415;
+  //output[0][0] = (float)((((int)output[0][0]-90) % 360 + 360) % 360);
 }
 
 static InitClass init("ToAngle", &ToAngle::Create, "Source/UserModules/ToAngle/");
