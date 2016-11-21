cdeeee2a9efc5f28e33deaee2ea8acb4f5501614
Daniel <pingisdaniel@gmail.com>
2016-11-21 18:01:01 +0100

    added armtestedr

---
+++ Source/UserModules/ArmTester/ArmTester.cc
@@ -0,0 +1,43 @@
+//
+//	MinimalModule.cc		This file is a part of the IKAROS project
+//
+//    Copyright (C) 2012 <Author Name>
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//    See http://www.ikaros-project.org/ for more information.
+//
+
+
+#include "ArmTester.h"
+
+using namespace ikaros;
+
+void
+ArmTester::Init()
+{
+  input_matrix = GetInputMatrix("INPUT");
+  output = GetOutputArray("OUTPUT");
+}
+
+void
+ArmTester::Tick()
+{
+  output[0] = 360.0 - (input_matrix[0][0] + 90.0);
+  output[1] = 135.0 - (input_matrix[0][1]);
+  output[2] = 0.0;
+}
+
+static InitClass init("ArmTester", &ArmTester::Create, "Source/UserModules/ArmTester/");
